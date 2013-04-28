d69c22018be446ff70871dcdb42be1a4b3535e1e
Michael Hagen <hagen-michael@gmx.de>
2013-04-28 11:59:23 +0200

    Small code optimization.

--- src/main/java/com/jtattoo/plaf/mint/MintBorders.java
+++ src/main/java/com/jtattoo/plaf/mint/MintBorders.java
@@ -77,7 +77,7 @@ public class MintBorders extends BaseBorders {
         }
 
         public Insets getBorderInsets(Component c) {
-            return new Insets(insets.top, insets.left, insets.bottom, insets.right);
+            return insets;
         }
 
         public Insets getBorderInsets(Component c, Insets borderInsets) {
@@ -89,7 +89,7 @@ public class MintBorders extends BaseBorders {
         }
 
         public boolean isBorderOpaque() {
-            return false;
+            return true;
         }
     } // class ButtonBorder
 
