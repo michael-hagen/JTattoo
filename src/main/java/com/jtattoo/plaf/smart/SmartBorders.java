643bfd0812c278e166a4dc4edd90586321adca1a
Michael Hagen <hagen-michael@gmx.de>
2013-04-28 11:59:38 +0200

    Small code optimization.

--- src/main/java/com/jtattoo/plaf/smart/SmartBorders.java
+++ src/main/java/com/jtattoo/plaf/smart/SmartBorders.java
@@ -112,7 +112,7 @@ public class SmartBorders extends BaseBorders {
         }
 
         public Insets getBorderInsets(Component c) {
-            return new Insets(insets.top, insets.left, insets.bottom, insets.right);
+            return insets;
         }
 
         public Insets getBorderInsets(Component c, Insets borderInsets) {
