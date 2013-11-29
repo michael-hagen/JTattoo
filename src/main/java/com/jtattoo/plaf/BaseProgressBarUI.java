b40b11cdb13c2966ccfabc05459d9d0fcda33760
Michael Hagen <hagen-michael@gmx.de>
2013-11-29 14:58:45 +0100

    Visual improvement displaying the text on horizontal progressbar.

--- src/main/java/com/jtattoo/plaf/BaseProgressBarUI.java
+++ src/main/java/com/jtattoo/plaf/BaseProgressBarUI.java
@@ -1,29 +1,30 @@
 /*
-* Copyright (c) 2002 and later by MH Software-Entwicklung. All Rights Reserved.
-*  
-* JTattoo is multiple licensed. If your are an open source developer you can use
-* it under the terms and conditions of the GNU General Public License version 2.0
-* or later as published by the Free Software Foundation.
-*  
-* see: gpl-2.0.txt
-* 
-* If you pay for a license you will become a registered user who could use the
-* software under the terms and conditions of the GNU Lesser General Public License
-* version 2.0 or later with classpath exception as published by the Free Software
-* Foundation.
-* 
-* see: lgpl-2.0.txt
-* see: classpath-exception.txt
-* 
-* Registered users could also use JTattoo under the terms and conditions of the 
-* Apache License, Version 2.0 as published by the Apache Software Foundation.
-*  
-* see: APACHE-LICENSE-2.0.txt
-*/
+ * Copyright (c) 2002 and later by MH Software-Entwicklung. All Rights Reserved.
+ *  
+ * JTattoo is multiple licensed. If your are an open source developer you can use
+ * it under the terms and conditions of the GNU General Public License version 2.0
+ * or later as published by the Free Software Foundation.
+ *  
+ * see: gpl-2.0.txt
+ * 
+ * If you pay for a license you will become a registered user who could use the
+ * software under the terms and conditions of the GNU Lesser General Public License
+ * version 2.0 or later with classpath exception as published by the Free Software
+ * Foundation.
+ * 
+ * see: lgpl-2.0.txt
+ * see: classpath-exception.txt
+ * 
+ * Registered users could also use JTattoo under the terms and conditions of the 
+ * Apache License, Version 2.0 as published by the Apache Software Foundation.
+ *  
+ * see: APACHE-LICENSE-2.0.txt
+ */
 
 package com.jtattoo.plaf;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import javax.swing.*;
@@ -55,27 +56,83 @@ public class BaseProgressBarUI extends BasicProgressBarUI {
     }
 
     /**
-     * The "selectionForeground" is the color of the text when it is painted
-     * over a filled area of the progress bar.
+     * The "selectionForeground" is the color of the text when it is painted over a filled area of the progress bar.
      */
     protected Color getSelectionForeground() {
         Object selectionForeground = progressBar.getClientProperty("selectionForeground");
         if (selectionForeground instanceof Color) {
-            return (Color)selectionForeground;
+            return (Color) selectionForeground;
         }
-	return super.getSelectionForeground();
+        return super.getSelectionForeground();
     }
 
     /**
-     * The "selectionBackground" is the color of the text when it is painted
-     * over an unfilled area of the progress bar.
+     * The "selectionBackground" is the color of the text when it is painted over an unfilled area of the progress bar.
      */
     protected Color getSelectionBackground() {
         Object selectionBackground = progressBar.getClientProperty("selectionBackground");
         if (selectionBackground instanceof Color) {
-            return (Color)selectionBackground;
+            return (Color) selectionBackground;
         }
-	return super.getSelectionBackground();
+        return super.getSelectionBackground();
+    }
+
+    private void paintString(Graphics g, int x, int y, int width, int height, int fillStart, int amountFull, Insets b) {
+        if (!(g instanceof Graphics2D)) {
+            return;
+        }
+
+        Graphics2D g2 = (Graphics2D) g;
+        String progressString = progressBar.getString();
+        g2.setFont(progressBar.getFont());
+        Point renderLocation = getStringPlacement(g2, progressString, x, y, width, height);
+        Rectangle oldClip = g2.getClipBounds();
+        
+        if (progressBar.getOrientation() == JProgressBar.HORIZONTAL) {
+            FontMetrics fm = Toolkit.getDefaultToolkit().getFontMetrics(progressBar.getFont());
+            int stringWidth = SwingUtilities.computeStringWidth(fm, progressString);
+            if (amountFull < renderLocation.x + stringWidth) {
+                g2.setColor(getSelectionBackground());
+                JTattooUtilities.drawString(progressBar, g2, progressString, renderLocation.x, renderLocation.y);
+            }
+            if (amountFull > renderLocation.x) {
+                g2.setColor(getSelectionForeground());
+                g2.clipRect(fillStart, y, amountFull, height);
+                JTattooUtilities.drawString(progressBar, g2, progressString, renderLocation.x, renderLocation.y);
+            }
+        } else { // VERTICAL
+            g2.setColor(getSelectionBackground());
+            AffineTransform rotate = AffineTransform.getRotateInstance(Math.PI / 2);
+            g2.setFont(progressBar.getFont().deriveFont(rotate));
+            renderLocation = getStringPlacement(g2, progressString, x, y, width, height);
+            JTattooUtilities.drawString(progressBar, g2, progressString, renderLocation.x, renderLocation.y);
+            g2.setColor(getSelectionForeground());
+            g2.clipRect(x, fillStart, width, amountFull);
+            JTattooUtilities.drawString(progressBar, g2, progressString, renderLocation.x, renderLocation.y);
+        }
+        g2.setClip(oldClip);
+    }
+
+    protected void paintString(Graphics g, int x, int y, int width, int height, int amountFull, Insets b) {
+        if (progressBar.getOrientation() == JProgressBar.HORIZONTAL) {
+            if (JTattooUtilities.isLeftToRight(progressBar)) {
+                if (progressBar.isIndeterminate()) {
+                    boxRect = getBox(boxRect);
+                    paintString(g, x, y, width, height, boxRect.x, boxRect.width, b);
+                } else {
+                    paintString(g, x, y, width, height, x, amountFull, b);
+                }
+            } else {
+                paintString(g, x, y, width, height, x + width - amountFull, amountFull, b);
+            }
+        } else {
+            if (progressBar.isIndeterminate()) {
+                boxRect = getBox(boxRect);
+                paintString(g, x, y, width, height, boxRect.y, boxRect.height, b);
+            } else {
+                paintString(g, x, y, width, height, y + height - amountFull,  amountFull, b);
+            }
+        }
     }
 
     protected void paintIndeterminate(Graphics g, JComponent c) {
@@ -116,22 +173,22 @@ public class BaseProgressBarUI extends BasicProgressBarUI {
             } else {
                 JTattooUtilities.fillVerGradient(g, colors, box.x + 2, box.y + 2, box.width - 4, box.height - 4);
             }
-        }
 
-        // Deal with possible text painting
-        if (progressBar.isStringPainted()) {
-            Object savedRenderingHint = null;
-            if (AbstractLookAndFeel.getTheme().isTextAntiAliasingOn()) {
-                savedRenderingHint = g2D.getRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING);
-                g2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, AbstractLookAndFeel.getTheme().getTextAntiAliasingHint());
-            }
-            if (progressBar.getOrientation() == JProgressBar.HORIZONTAL) {
-                paintString(g2D, b.left, b.top, barRectWidth, barRectHeight, box.width, b);
-            } else {
-                paintString(g2D, b.left, b.top, barRectWidth, barRectHeight, box.height, b);
-            }
-            if (AbstractLookAndFeel.getTheme().isTextAntiAliasingOn()) {
-                g2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, savedRenderingHint);
+            // Deal with possible text painting
+            if (progressBar.isStringPainted()) {
+                Object savedRenderingHint = null;
+                if (AbstractLookAndFeel.getTheme().isTextAntiAliasingOn()) {
+                    savedRenderingHint = g2D.getRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING);
+                    g2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, AbstractLookAndFeel.getTheme().getTextAntiAliasingHint());
+                }
+                if (progressBar.getOrientation() == JProgressBar.HORIZONTAL) {
+                    paintString(g2D, b.left, b.top, barRectWidth, barRectHeight, box.width, b);
+                } else {
+                    paintString(g2D, b.left, b.top, barRectWidth, barRectHeight, box.height, b);
+                }
+                if (AbstractLookAndFeel.getTheme().isTextAntiAliasingOn()) {
+                    g2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, savedRenderingHint);
+                }
             }
         }
     }
