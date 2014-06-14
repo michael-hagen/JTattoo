c077e2c24c470c7c1194980f5e6173c012ff1d41
Michael Hagen <hagen-michael@gmx.de>
2014-06-14 14:45:34 +0200

    New interface to support custom title panes.

--- src/main/java/com/jtattoo/plaf/hifi/HiFiRootPaneUI.java
+++ src/main/java/com/jtattoo/plaf/hifi/HiFiRootPaneUI.java
@@ -24,7 +24,6 @@
 package com.jtattoo.plaf.hifi;
 
 import com.jtattoo.plaf.BaseRootPaneUI;
-import com.jtattoo.plaf.BaseTitlePane;
 import javax.swing.JComponent;
 import javax.swing.JRootPane;
 import javax.swing.plaf.ComponentUI;
@@ -38,7 +37,7 @@ public class HiFiRootPaneUI extends BaseRootPaneUI {
         return new HiFiRootPaneUI();
     }
 
-    public BaseTitlePane createTitlePane(JRootPane root) {
+    public JComponent createTitlePane(JRootPane root) {
         return new HiFiTitlePane(root, this);
     }
 }
