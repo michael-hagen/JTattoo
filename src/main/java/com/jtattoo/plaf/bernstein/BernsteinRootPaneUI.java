b1fab1f143d1e2f59c7df5ae66a9a8c1eb59dffa
Michael Hagen <hagen-michael@gmx.de>
2014-06-14 14:44:34 +0200

    New interface to support custom title panes.

--- src/main/java/com/jtattoo/plaf/bernstein/BernsteinRootPaneUI.java
+++ src/main/java/com/jtattoo/plaf/bernstein/BernsteinRootPaneUI.java
@@ -24,7 +24,6 @@
 package com.jtattoo.plaf.bernstein;
 
 import com.jtattoo.plaf.BaseRootPaneUI;
-import com.jtattoo.plaf.BaseTitlePane;
 import javax.swing.JComponent;
 import javax.swing.JRootPane;
 import javax.swing.plaf.ComponentUI;
@@ -38,7 +37,7 @@ public class BernsteinRootPaneUI extends BaseRootPaneUI {
         return new BernsteinRootPaneUI();
     }
 
-    public BaseTitlePane createTitlePane(JRootPane root) {
+    public JComponent createTitlePane(JRootPane root) {
         return new BernsteinTitlePane(root, this);
     }
 }
